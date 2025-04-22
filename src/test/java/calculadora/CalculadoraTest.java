f29e220c5cf0827a8fb16cdc10947ece28771819
1072410296 <1072410296@UNS07DT98816.animaedu.intranet>
2025-04-22 10:20:55 -0300

    Revert "fix test"
    This reverts commit dbc0acc5fc7b485ffeff2c5c62e0604695c8bf25.

--- src/test/java/calculadora/CalculadoraTest.java
+++ src/test/java/calculadora/CalculadoraTest.java
@@ -12,7 +12,7 @@ class CalculadoraTest {
     
 @Test
     void testGetSOMA(){
-        Calculadora calculadora = new Calculadora(4.0, 2.0);
+        Calculadora calculadora = new Calculadora(4.0, 20.0);
         double retornoEsperado = 6.0;
         double retornoFeito = calculadora.getSoma();
         assertEquals(retornoEsperado, retornoFeito, 0);
