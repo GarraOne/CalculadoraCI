891aa2c6525376e4900a1a1876b80cd6b0de692c
VictorLSRodrigues <victor.scheuer.rodrigues@gmail.com>
2025-04-29 10:03:44 -0300

    add more tests

--- src/test/java/calculadora/CalculadoraTest.java
+++ src/test/java/calculadora/CalculadoraTest.java
@@ -19,5 +19,43 @@ class CalculadoraTest {
         double retornoEsperado = 2.0;
         double retornoFeito = calculadora.getDiferenca();
         assertEquals(retornoEsperado, retornoFeito, 0);
+        
     }
+     @Test
+    void testGetProduto() {
+
+        Calculadora calculadora = new Calculadora(4.0, 2.0);
+
+        double retornoEsperado = 8.0;
+
+        double retornoFeito = calculadora.getProduto();
+
+        assertEquals(retornoEsperado, retornoFeito, 0);
+    }
+    @Test
+    void testGetQuociente() {
+
+        Calculadora calculadora = new Calculadora(4.0, 2.0);
+
+        double retornoEsperado = 2.0;
+
+        double retornoFeito = calculadora.getQuociente();
+
+        assertEquals(retornoEsperado, retornoFeito, 0);
+    }
+    @Test
+    void testgetgetExponencial(){
+       Calculadora calculadora = new Calculadora(2.0, 3.0);
+        double retornoEsperado = 8.0;
+        double retornoFeito = calculadora.getExponencial();
+        assertEquals(retornoEsperado,retornoFeito,0);  
+    }
+    
+    @Test
+    void testGetVazio(){
+        Calculadora calculadora = new Calculadora();
+        double retornoEsperado = 0.0;
+        double retornoFeito = calculadora.getSoma();
+        assertEquals(retornoEsperado, retornoFeito, 0);
+}
 }
